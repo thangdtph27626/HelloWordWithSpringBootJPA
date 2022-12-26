234549d98393c9885d268615874addee452d2e42
thangdtph27626 <109157942+thangdtph27626@users.noreply.github.com>
2022-12-26 14:21:02 +0700

    firt commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
--- src/main/java/com/example/demo/controller/Hello.java
+++ src/main/java/com/example/demo/controller/Hello.java
@@ -1,7 +1,8 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Map;
 
@@ -9,9 +10,9 @@ import java.util.Map;
  * @author thangdt
  */
 @Controller
-public class hello {
+public class Hello {
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/")
     public String welcome(Map<String, Object> model) {
         model.put("message", "hello");
         return "hello";
